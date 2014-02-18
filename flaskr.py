d22c835c7b569e585fc01b82f03b035c607b9d35
Kwanggoo <kwang8992@gmail.com>
2014-02-18 23:12:04 +0900

    *flaskr 수정본(1차)

--- flaskr.py
+++ flaskr.py
@@ -1,6 +1,5 @@
 # all the imports
-import os
-from sqlite3 import dbapi2 as sqlite3
+import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
              abort, render_template, flash 
 
