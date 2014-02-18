76ddc2f92c09213db47916f60dc6e23adb7c4bf4
Kwanggoo <kwang8992@gmail.com>
2014-02-18 23:30:25 +0900

    *indent 수정완료

--- flaskr.py
+++ flaskr.py
@@ -5,71 +5,71 @@ from flask import Flask, request, session, g, redirect, url_for, \
 
 
 # configuration
-         DATABASE = '/tmp/flaskr.db'
-         DEBUG = True
-         SECRET_KEY = 'deelopment key'
-         USERNAME = 'admin'
-         PASSWORD = 'default'
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'deelopment key'
+USERNAME = 'admin'
+PASSWORD = 'default'
 
 # create our little application :)
-    app = Flask(__name__)
+app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-    def connect_db():
-        return sqlite3.connect(app.config['DATABASE'])
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
-        def init_db():
-            with closing(connect_db()) as db:
-                with app.open_resource('schema.sql') as f:
-    db.cursor().executescript(f.read())
-    db.commit()
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
-    @app.route('/')
-    def show_entries():
-        cur = g.db.execute('select title, text from entries order by id desc')
-        entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-        return render_template('show_entries.html', entries=entries)    
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)    
 
-        @app.route('/add', methods=['POST'])
-        def add_entry():
-            if not session.get('logged_in'):
-                abort(401)
-                g.db.execute('insert into entries (title, text) values (?, ?)',
-                        [request.form['title'], request.form['text']])
-g.db.commit()
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                [request.form['title'], request.form['text']])
+    g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
-    @app.route('/login', methods=['GET','POST'])
-    def login():
-        error = None
-        if request.method == 'POST':
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
-        error = 'Invalid username'
+            error = 'Invalid username'
         elif request.form['password'] != app.config['PASSWORD']:
-        error = 'Invalid password'
+            error = 'Invalid password'
         else:
-        session['logged_in'] = Trueflash('You were logged in')
-        return redirect(url_for('show_entries'))
-        return render_template('login.html', error=error)
+            session['logged_in'] = Trueflash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
 
-        @app.route('/logout')
-        def logout():
-            session.pop('logged_in', None)
-            flash('You were logged out')
-            return redirect(url_for('show_entries'))    
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))    
 
-            @app.before_request
-            def before_request():
-                g.db = connect_db()
+@app.before_request
+def before_request():
+    g.db = connect_db()
 
-                @app.teardown_request
-                def teardown_request(exception):
-                    g.db.close()    
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()    
 
-                    if __name__ == '__main__':
+if __name__ == '__main__':
     init_db()
-app.run()
+    app.run()
 
